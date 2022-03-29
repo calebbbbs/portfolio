9b0f9a9b330e4039657a0148bb28ef04eb5b06f8
calebbbbs <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>
2022-03-29 13:02:00 -0500

    working contact me page

--- frontend/src/components/Bio.jsx
+++ frontend/src/components/Bio.jsx
@@ -1,18 +1,20 @@
 function Bio() {
   return (
     <>
-      <div className='container mx-auto max-w-lg min-h-min rounded overflow-hidden shadow-lg my-24 bg-white opacity-90'>
-        <div className='px-2 py-4'>
-          <p className='text-left text-2xl text-cyan-500'>
-            I am a full-stack JavaScript software engineer making a career
-            change from hospitality and labor. I am passionate about creating
-            dynamic user experiences and learning new technologies. I want to
-            merge my passions together to create powerful applications designed
-            with the user in mind every step of the way. I have experience
-            leading a team, with the AGILE/Scrum dynamic lending itself to my
-            natural disposition. My training has been with NodeJs, React,
-            JavaScript, TypeScript, and AWS.
-          </p>
+      <div className='bg-gray-300 box'>
+        <div className='box mx-auto max-w-lg min-h-min rounded overflow-hidden shadow-lg bg-white opacity-90'>
+          <div className='px-2 py-4'>
+            <p className='text-left text-2xl text-slate-700'>
+              I am a full-stack JavaScript software engineer making a career
+              change from hospitality and labor. I am passionate about creating
+              dynamic user experiences and learning new technologies. I want to
+              merge my passions together to create powerful applications
+              designed with the user in mind every step of the way. I have
+              experience leading a team, with the AGILE/Scrum dynamic lending
+              itself to my natural disposition. My training has been with
+              NodeJs, React, JavaScript, TypeScript, and AWS.
+            </p>
+          </div>
         </div>
       </div>
     </>
